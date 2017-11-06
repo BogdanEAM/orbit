4a6d9b0b88126d918414eb7772121b0767684a1b
Joe Hegarty <joe.hegarty@gmail.com>
2017-11-05 18:24:09 -0800

    Initial

---
+++ orbit-core/src/main/kotlin/cloud/orbit/util/StringUtils.kt
@@ -0,0 +1,64 @@
+/*
+ Copyright (C) 2017 Electronic Arts Inc.  All rights reserved.
+
+ Redistribution and use in source and binary forms, with or without
+ modification, are permitted provided that the following conditions
+ are met:
+
+ 1.  Redistributions of source code must retain the above copyright
+     notice, this list of conditions and the following disclaimer.
+ 2.  Redistributions in binary form must reproduce the above copyright
+     notice, this list of conditions and the following disclaimer in the
+     documentation and/or other materials provided with the distribution.
+ 3.  Neither the name of Electronic Arts, Inc. ("EA") nor the names of
+     its contributors may be used to endorse or promote products derived
+     from this software without specific prior written permission.
+
+ THIS SOFTWARE IS PROVIDED BY ELECTRONIC ARTS AND ITS CONTRIBUTORS "AS IS" AND ANY
+ EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ DISCLAIMED. IN NO EVENT SHALL ELECTRONIC ARTS OR ITS CONTRIBUTORS BE LIABLE FOR ANY
+ DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF
+ THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+package cloud.orbit.util
+
+import java.security.SecureRandom
+
+object StringUtils {
+    private val secureRandom = SecureRandom()
+
+    private val base64AllowedCharacters = charArrayOf(
+            'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M',
+            'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z',
+            'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm',
+            'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
+            '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '-', '_'
+    )
+
+    @JvmStatic
+    @JvmOverloads
+    fun secureBase64Random(numBits: Int = 128): String {
+        tailrec fun processIdentity(remainingBits: Int, outSb: StringBuilder) {
+            if (remainingBits > 0) {
+                val nextIntBound = if (remainingBits > 6) 64 else (1 shl remainingBits)
+                val nextInt = secureRandom.nextInt(nextIntBound)
+                outSb.append(base64AllowedCharacters[nextInt])
+                processIdentity(remainingBits - 6, outSb)
+            }
+        }
+        val result = StringBuilder()
+        processIdentity(numBits, result)
+        return result.toString()
+    }
+}
+
+fun String.Companion.secureBase64Random(numBits: Int = 128): String = StringUtils.secureBase64Random(numBits)
+
+
+
